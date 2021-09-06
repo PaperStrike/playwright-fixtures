28815df7873e9174171e9921d238811f41782310
华 <1395348685z@gmail.com>
2021-09-06 21:37:27 +0800

    Set avaliable resolved fixture values
    - Fix peer fixtures being completely ignored when handling a fixture function

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
-type KeyValue = Record<string, unknown>;
+type KeyValue = {
+  // Everything other than functions.
+  // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
+  [K: string]: string | number | boolean | any[] | null | undefined | bigint | symbol | void | {};
+};
 
-type TestFixture<R, Args extends KeyValue> = (
+type FixtureFunction<R, Args extends KeyValue> = (
   args: Args,
   use: (r: R) => Promise<void>,
 ) => Promise<void> | void;
 
-type TestFixtureValue<R, Args extends KeyValue> = R | TestFixture<R, Args>;
+type FixtureValue<R, Args extends KeyValue> = R | FixtureFunction<R, Args>;
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type Fixtures<T extends KeyValue, PT extends KeyValue = {}> = {
-  [K in keyof PT]?: TestFixtureValue<PT[K], PT & T>;
+  [K in keyof PT]?: FixtureValue<PT[K], PT & T>;
 } & {
-  [K in keyof T]: TestFixtureValue<T[K], PT & T>;
+  [K in keyof T]: FixtureValue<T[K], PT & T>;
 };
 
 type BaseTest = (name: string, inner: (...args: unknown[]) => Promise<void> | void) => unknown;
@@ -27,11 +31,11 @@ type Test<Args extends KeyValue, B extends BaseTest> = Pick<B, keyof B> & {
   ): Test<Args & T, B>;
 };
 
-const prepareFixtures = async <T extends KeyValue, Args extends KeyValue>(
-  base: Args,
-  init: Fixtures<T, Args>,
-): Promise<[Args & T, Promise<void>[], () => void]> => {
-  const extend = {} as Partial<T>;
+const prepareFixtures = async <T extends KeyValue, PT extends KeyValue>(
+  base: PT,
+  init: Fixtures<T, PT>,
+): Promise<[PT & T, Promise<void>[], () => void]> => {
+  const extend: Partial<T> = {};
   let useResolve: () => void;
   let usePromise: Promise<void>;
   await new Promise<void>((construct) => {
@@ -39,11 +43,12 @@ const prepareFixtures = async <T extends KeyValue, Args extends KeyValue>(
   });
   const finishJobs: Promise<void>[] = [];
   const prepareJobs = Object.entries(init)
-    .map(<K extends keyof T>([key, fixtureValue]: [K, Fixtures<T, Args>[K]]) => (
+    .map(<K extends keyof T>([key, fixtureValue]: [K, FixtureValue<T[K], PT & T>]) => (
       new Promise<void>((prepareValueResolve) => {
         /**
          * Check if it is callable.
          * Hard to be reliable and fast at the same time.
+         * E.g., classes are functions, too.
          */
         if (typeof fixtureValue === 'function') {
           const useValue = async (value: T[K]) => {
@@ -52,12 +57,20 @@ const prepareFixtures = async <T extends KeyValue, Args extends KeyValue>(
             await usePromise;
           };
           finishJobs.push(
-            // Package to promise, another resolve in case of it dont use `useValue`.
-            Promise.resolve(fixtureValue(base, useValue))
+            /**
+             * Package to promise, chain with another resolve in case of
+             * the fixture function finishes without using `useValue`.
+             *
+             * Assert `extend` type `T` to make it possible for users to use peer fixtures
+             * as in Playwright's official docs.
+             * @TODO filter out constants before handling these fixture functions.
+             * @see [Test fixtures - Advanced: fixtures | Playwright]{@link https://playwright.dev/docs/test-fixtures/#overriding-fixtures}
+             */
+            Promise.resolve(fixtureValue({ ...base, ...extend as T }, useValue))
               .then(prepareValueResolve),
           );
         } else {
-          extend[key] = fixtureValue as T[K];
+          extend[key] = fixtureValue;
           prepareValueResolve();
         }
       })
